4abebdcc14ea74d99cb8f4106d7becd1b4205548
feysel <feyselmifta0808@outlook.com>
2025-12-01 11:57:03 +0300

    Clean repo and apply .gitignore

---
+++ Desktop/Mrkuze-BookStore/frontend/src/routers/router.jsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "/",
+                element: <h1>Home</h1>,
+            }
+        ]
+    },
+]);
+
+export default router;
